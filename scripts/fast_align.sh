28aea7e8a0bff0de6ade27eb4b06d8a0f7c1c33c
Uzumaki Naruto <nguyenduyhanlam@yahoo.com.vn>
2021-12-18 00:28:32 +0700

    update

--- scripts/fast_align.sh
+++ scripts/fast_align.sh
@@ -24,16 +24,17 @@ target_path=$2
 source_name=${1##*/}
 target_name=${2##*/}
 direction=$3
+resultHandle_path=$4
 
 # create format used for fastalign
-paste ${source_path} ${target_path} | sed -E 's/\t/ ||| /g' > ${source_name}_${target_name}
-paste ${target_path} ${source_path} | sed -E 's/\t/ ||| /g' > ${target_name}_${source_name}
+paste ${source_path} ${target_path} | sed -E 's/\t/ ||| /g' > ${resultHandle_path}/${source_name}_${target_name}
+paste ${target_path} ${source_path} | sed -E 's/\t/ ||| /g' > ${resultHandle_path}/${target_name}_${source_name}
 
 # remove lines which have an empty source or target
-sed -e '/^ |||/d' -e '/||| $/d' ${source_name}_${target_name} > ${source_name}_${target_name}.clean
-sed -e '/^ |||/d' -e '/||| $/d' ${target_name}_${source_name} > ${target_name}_${source_name}.clean
+sed -e '/^ |||/d' -e '/||| $/d' ${resultHandle_path}/${source_name}_${target_name} > ${source_name}_${target_name}.clean
+sed -e '/^ |||/d' -e '/||| $/d' ${resultHandle_path}/${target_name}_${source_name} > ${target_name}_${source_name}.clean
 
 # align in both directions
-${FASTALIGN_DIR}/build/fast_align -i ${source_name}_${target_name}.clean -p ${direction}.model -d -o -v > ${direction}.talp 2> ${direction}.error
-${FASTALIGN_DIR}/build/fast_align -i ${target_name}_${source_name}.clean -p ${direction}.reverse.model -d -o -v > ${direction}.reverse.talp 2> ${direction}.reverse.error
+${FASTALIGN_DIR}/build/fast_align -i ${resultHandle_path}/${source_name}_${target_name}.clean -p ${direction}.model -d -o -v > ${direction}.talp 2> ${direction}.error
+${FASTALIGN_DIR}/build/fast_align -i ${resultHandle_path}/${target_name}_${source_name}.clean -p ${direction}.reverse.model -d -o -v > ${direction}.reverse.talp 2> ${direction}.reverse.error
 
